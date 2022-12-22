2df07c4352095da7964ffa1060a3cea55ce94cfb
paras9sid <sidharthjainp9@gmail.com>
2022-12-23 01:07:33 +0530

    Events tutorial on codesandbox done

--- cart/src/App.js
+++ cart/src/App.js
@@ -3,11 +3,29 @@ import CartItem from './CartItem';
 
 
 function App() {
+
+  // events 
+
+  
+    function showAlert() {
+      alert("Hello!");
+    }
+  
+    function handleInputchange(e) {
+      console.log(e.target.value);
+    }
+  
   return (
     <div className="App">
-     <h1>Cart</h1>
+      
+    <button onClick={showAlert}>Show Alert</button>
+
+    <input onChange={handleInputchange} />
+
+
+     {/* <h1>Cart</h1> */}
      {/* Component imported above top and calling below */}
-     <CartItem />
+     {/* <CartItem /> */}
     </div>
   );
 }
