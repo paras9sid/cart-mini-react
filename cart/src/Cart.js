009264aaae0c3896da601625ef1a9a98d7f12b3d
paras9sid <sidharthjainp9@gmail.com>
2022-12-23 22:10:10 +0530

    Rendering list and map function done

--- cart/src/Cart.js
+++ cart/src/Cart.js
@@ -4,11 +4,20 @@ import CartItem from "./CartItem";
 
 class Cart extends React.Component {
     render(){
+
+        //rendering list
+        const arr = [1,2,3,4,5];
         return (
-            <div className="cart">                
+            <div className="cart">   
+                {/* { //rendering list - 12345
+                arr }               */}
+                {arr.map( (item) => {
+                    return item + 5;
+                    //result = 678910 in browser arr every item will be added with +5 1+5=6..so on
+                })} 
+                {/* <CartItem />
                 <CartItem />
-                <CartItem />
-                <CartItem />
+                <CartItem /> */}
             </div>
         );
     }
