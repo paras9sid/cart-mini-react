8e66ce7ea2bfcad434cb7b8c9d31478bdd3984d2
paras9sid <sidharthjainp9@gmail.com>
2022-12-24 23:22:47 +0530

    Navbar component added

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import Cart from './Cart';
+import Navbar from './Navbar';
 
 
 function App() {
-
-  // // events practice
-  //   function showAlert() {
-  //     alert("Hello!");
-  //   }
-  //   function handleInputchange(e) {
-  //     console.log(e.target.value); // will print word by word in console
-  //   }
-  
   return (
     <div className="App">
-
-
-    {/* for events written above return */}
-    {/* <button onClick={showAlert}>Show Alert</button>
-    <input onChange={handleInputchange} /> */}
-
-
-     {/* <h1>Cart</h1> */}
-     {/* Component imported above top and calling below */}
-     <Cart />
+      <Navbar />
+      <Cart />
     </div>
   );
 }
