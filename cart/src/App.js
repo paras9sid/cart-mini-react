3b5f568b99c362d6f9013589428b06452c3b4488
paras9sid <sidharthjainp9@gmail.com>
2023-01-06 15:59:46 +0530

    Fetch data from cloud firestore updated latest version

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,37 +1,80 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+// import { db } from "./index";
+import { firestore } from "./firebase";
 
 class App extends React.Component {
   constructor(){
     
       super(); // calling componenet of parent class --otherwise page display will not render
       this.state = {
-          products:[
-              {
-                  price : 99,
-                  title : 'Watch',
-                  qty : 1,
-                  img:'https://i.ebayimg.com/images/g/6L8AAOSw2~NcGoaq/s-l500.jpg',
-                  id:1
-              },
-              {
-                  price : 999,
-                  title : 'Mobile Phone',
-                  qty : 10,
-                  img:'https://m.media-amazon.com/images/I/610pghkO81L._SX522_.jpg',
-                  id:2
-              },
-              {
-                  price : 9999,
-                  title : 'Laptop',
-                  qty : 5,
-                  img:'https://www.apple.com/v/macbook-pro-14-and-16/b/images/overview/hero/intro__ewz1ro7xs14y_large.jpg',
-                  id:3
-              }
-          ]
+          products:[], // default empty array now as we are fetching data from firebase db
+          loading: true,
       }
     }
+
+    //function to change 
+//  async componentDidMount() {
+//   //this is a realtime listener if you change anything in firebase ui will automatically updated 
+//     //  const q = query(
+//     //    collection(db, "products"),
+//     //    where("price", ">", 0),
+//     //    orderBy("price")
+//     //  );
+//     //  const unsub = await onSnapshot(q, (querySnapshot) => {
+//     //    const getProducts = [];
+//     //    querySnapshot.forEach((doc) => {
+//     //      const product = doc.data();
+//     //      product.id = doc.id;
+//     //      getProducts.push(product);
+//     //    });
+//     //    console.log(getProducts);
+//     //    this.setState({ products: getProducts, loading: false });
+//     //  });
+
+//     const prod = [];
+//     onSnapshot(collection(db, 'posts'), (snapshot) => {
+//       prod(snapshot.docs.map((doc) => {
+//           return {
+//               id: doc.id,
+//               data: doc.data()
+//           }
+//       })
+//       )
+//   })
+
+    // const subs = db
+    //               .collection("products")
+    //               .onSnapshot((querySnapshot)=>{
+    //                   querySnapshot.forEach((doc)=>{
+    //                       prod.push({
+    //                         ...doc.data(),
+    //                         key: doc.id,
+    //                       });
+    //                       console.log(prod);
+    //                 this.setState({
+    //                    products: prod, 
+    //                    loading: false
+    //                  });
+    //                   })
+    //               })
+  //  }
+
+  componentDidMount() {
+    
+      firestore
+      .collection("products")
+      .onSnapshot(snapshot => {
+        const products = snapshot.docs.map(doc => {
+          const data = doc.data();
+          data["id"] = doc.id;
+          return data;
+        });
+        this.setState({ products: products, loading: false });
+      });
+  }
+
   handleIncreaseQuantity = (product) => {
       console.log("Please inc the qty" , product);
       const {products} = this.state;
@@ -45,6 +88,17 @@ class App extends React.Component {
       })
 
   }
+
+  // handleIncreaseQuantity = async (product) => {
+  //   let { products } = this.state;
+  //   const index = products.indexOf(product);
+
+  //   const docRef = doc(collection(db, "products"), products[index].id);
+  //   await updateDoc(docRef, {
+  //     qty: products[index].qty + 1,
+  //   });
+  // };
+
   handleDecreaseQuantity = (product) => {
       console.log("Please inc the qty" , product);
       const {products} = this.state;
@@ -62,6 +116,18 @@ class App extends React.Component {
       })
 
   }
+  
+  // handleDecreaseQuantity = async (product) => {
+  //   let { products } = this.state;
+  //   const index = products.indexOf(product);
+  //   if (products[index].qty !== 0) {
+  //     const docRef = doc(collection(db, "products"), products[index].id);
+  //     await updateDoc(docRef, {
+  //       qty: products[index].qty - 1,
+  //     });
+  //   }
+  // };
+
   handleDeleteProduct = (id) => {
       const { products } = this.state;
       const items = products.filter((item)=> item.id !== id); //[{}] will return array of objects(products) of items whose id !== id
@@ -70,6 +136,18 @@ class App extends React.Component {
           products : items
       })    
   }
+
+  // handleDeleteProduct = (productToDelete) => {
+  //   const docRef = doc(collection(db, "products"), productToDelete);
+  //   deleteDoc(docRef)
+  //     .then(() => {
+  //       console.log("product deleted");
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
+
 
    getCartCount = () => {
     const { products } = this.state;
@@ -83,16 +161,21 @@ class App extends React.Component {
     return count;
   }
 
-  getCartTotal = () =>{
+   getCartTotal = () => {
     const {products} = this.state;
-
-    let cartTotal = 0;
-
-    products.map((product)=>{
-      cartTotal += product.qty * product.price;
-    });
-    return cartTotal;
-  }
+    
+      let cartTotal = 0;
+  
+      products.map( (product) => {
+  
+        if(product.qty > 0){
+          cartTotal += product.qty * product.price;
+        }    
+        return '';
+      });
+  
+      return cartTotal;
+    }
   render(){
     const { products } = this.state;
     return (
@@ -112,3 +195,163 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+// import React from 'react';
+// import Cart from './Cart';
+// import Navbar from './Navbar';
+
+// import {
+//   doc,
+//   set,
+//   addDoc,
+//   setDoc,
+//   collection,
+//   updateDoc,
+//   deleteDoc,
+//   onSnapshot,
+//   query,
+//   where,
+//   orderBy,
+//   // doc,
+// } from "firebase/firestore";
+// import { db } from "./index";
+// import { firestore } from "../firebase";
+
+// // import { async } from '@firebase/util';
+// // import { app } from 'firebase-admin';
+
+// class App extends React.Component {
+//   constructor(){
+//         super();
+//         this.state = {
+//           products:[],
+//           loading : true
+//         }
+//   }
+
+//   //function to change  -- with new version updated code
+//   async componentDidMount() {
+//   //this is a realtime listener if you change anything in firebase ui will automatically updated 
+//     const q = query(
+//       collection(db, "products"),
+//       where("price", ">", 0),
+//       orderBy("price")
+//     );
+//     const unsub = await onSnapshot(q, (querySnapshot) => {
+//       const getProducts = [];
+//       querySnapshot.forEach((doc) => {
+//         const product = doc.data();
+//         product.id = doc.id;
+//         getProducts.push(product);
+//       });
+//     //  console.log(getProducts);
+//       this.setState({ 
+//       products: getProducts,
+//       loading: false 
+//       });
+//     });
+//   }
+    
+
+//   handleIncreaseQuantity = async(product) => { 
+      
+//       const { products } = this.state;
+//       const index = products.indexOf(product);
+//       const docRef = doc(collection(db, "products"), products[index].id);
+//       await updateDoc(docRef, {
+//         qty: products[index].qty + 1,
+//       });
+//   };
+  
+//   handleDecreaseQuantity = async(product) => {
+      
+//       const { products } = this.state;
+//       const index = products.indexOf(product);
+
+//       if( products[index].qty ===0 ){
+//         const docRef = doc(collection(db, "products"), products[index].id);
+//         await updateDoc(docRef, {
+//           qty: products[index].qty - 1,
+//         });
+//       }
+//   }
+  
+//   handleDeleteProduct = (id) => {
+//       const { products } = this.state;
+//       const items = products.filter((item)=> item.id !== id);// [{}] will pass the array whose item id is not = to the product id
+//       this.setState({
+//           products:items
+//       });
+//   }
+
+//   getCartCount = () => {
+//     const {products} = this.state
+
+//     let count = 0;
+
+//     products.forEach((product) => {
+//       count += product.qty;
+//     });
+
+//     return count;
+//   }    
+
+//   getCartTotal = () => {
+//     const {products} = this.state;
+
+//     let cartTotal = 0;
+
+//     products.map( (product) => {
+
+//       if(product.qty > 0){
+//         cartTotal += product.qty * product.price;
+//       }    
+//       return '';
+//     });
+
+//     return cartTotal;
+//   }
+
+//   // addProduct = async(product) => {
+
+//   //   const { products } = this.state;
+//   //   const docRef = doc(collection(db, "products"));
+//      // Add a new document in collection "products"
+//     // docRef.set({
+//     //   img: '',
+//     //   price: 12000,
+//     //   qty: 1,
+//     //   title: "Washing Machine"
+//     // })
+//     //   .then(() => {
+//     //     console.log("Document successfully written!");
+//     //   })
+//     //   .catch((error) => {
+//     //     console.error("Error writing document: ", error);
+//     //   });
+
+//   // }
+//   render () {
+//     const {products,loading} = this.state;
+//     return (
+//       <div className="App">
+//         <h1>Cart</h1>
+//         {/* count dynamic in navbar componenet */}
+//         <Navbar count = {this.getCartCount()}/> 
+//         {/* <button onClick={this.addProduct} style={ { padding:10,fontSize:20 } }>Add a product</button> */}
+//         <Cart 
+//           products = {products}
+//           onIncreaseQuantity = { this.handleIncreaseQuantity }
+//           onDecreaseQuantity = { this.handleDecreaseQuantity }
+//           onDeleteProduct = {this.handleDeleteProduct}
+
+//         />
+//         {loading && <h1>Loading Products...</h1>}
+
+//         <div style={ { padding:10,fontSize:20 } }>Total : {this.getCartTotal()}</div>
+//       </div>
+//       );
+//   }
+// }
+// export default App;
---
+++ cart/src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+    //firebse config , remove everytime you git push
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+
