4354539528d16ad7e6c0d107a216d582257c4a63
paras9sid <sidharthjainp9@gmail.com>
2023-01-09 16:33:44 +0530

    update & decrease qty in cloud firestore done

--- cart/src/App.js
+++ cart/src/App.js
@@ -25,21 +25,44 @@ class App extends React.Component {
           data["id"] = doc.id;
           return data;
         });
-        this.setState({ products: products, loading: false });
+      
+        this.setState({
+           products: products,
+           loading: false 
+        });
+      
       });
   }
 
   handleIncreaseQuantity = (product) => {
-      console.log("Please inc the qty" , product);
+      // console.log("Please inc the qty" , product);
       const {products} = this.state;
       const index = products.indexOf(product);
 
-      products[index].qty +=1;
+      //increasing qty in state
+      // products[index].qty +=1;
 
-      this.setState({
-          // products: products   // shorthand means = products 
-          products // using shorthand of above stmt
+
+      // this.setState({
+      //     // products: products   // shorthand means = products 
+      //     products // using shorthand of above stmt
+      // })
+
+      //increase qty in firebase cloud directly 
+      //using id below we get the ref of that product which we are increasing
+      const docRef = firestore.collection('products').doc(products[index].id)
+
+      docRef
+      .update({
+        qty : products[index].qty +1,
       })
+      .then(() => {
+        console.log('document updated successfully');
+      })
+      .catch((error) =>  {
+        console.log('error',error);
+      })
+
 
   }
 
@@ -52,11 +75,26 @@ class App extends React.Component {
           return;
       }
 
-      products[index].qty -=1;
+      // //decreasing the state
+      // products[index].qty -=1;
 
-      this.setState({
-          // products: products   // shorthand means = products 
-          products // using shorthand of above stmt
+      // this.setState({
+      //     // products: products   // shorthand means = products 
+      //     products // using shorthand of above stmt
+      // })
+
+      //decreasing qty with ref of id of product
+      const docRef = firestore.collection('products').doc(products[index].id)
+
+      docRef
+      .update({
+        qty : products[index].qty -1,
+      })
+      .then(() => {
+        console.log('document updated successfully qty decreased');
+      })
+      .catch((error) =>  {
+        console.log('error',error);
       })
 
   }
@@ -119,14 +157,15 @@ class App extends React.Component {
 
 
  render(){
-    const { products ,loading} = this.state;
+    const { products ,loading } = this.state;
     return (
       <div className="App">
         {/* making count in navbar dynamic from fixed/static by using below written function */}
         <Navbar count = {this.getCartCount()}/>
 
         {/* adding products in firebase - lecture */}
-        <button onClick={this.addProduct} style={{padding :20 , fontSize:20}}>Add a product</button>
+        {/* button commented - updating in db lecture */}
+        {/* <button onClick={this.addProduct} style={{padding :20 , fontSize:20}}>Add a product</button> */}
 
         <Cart 
          products = {products}
